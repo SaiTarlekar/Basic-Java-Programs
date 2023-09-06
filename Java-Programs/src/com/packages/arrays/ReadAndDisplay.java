d677c8e70be4bdb4f1a529cba768a5ac340aab77
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-06 14:44:31 +0530

    Commit

---
+++ Java-Programs/src/com/packages/arrays/ReadAndDisplay.java
@@ -0,0 +1,34 @@
+package com.packages.arrays;
+import java.util.Scanner;
+
+public class ReadAndDisplay {
+
+	
+	static int readArrays() {
+		
+		
+		
+		
+		return 1;
+	}
+	
+	static void displayArrays() {
+		
+	}
+	
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello!! \nDo you know size of the Array (Y/N): ");
+		char ch = scan.next().charAt(0);
+		
+		switch(ch) {
+		case 'Y':
+			break;
+		
+		case 'N':
+			break;
+		}
+		
+	}
+}
