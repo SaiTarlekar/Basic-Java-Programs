52980e84d14cc95853a5c13fa97285d6f655aef2
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-06 20:00:23 +0530

    Commit

--- Java-Programs/src/com/packages/methods/CalculateNetSalary.java
+++ Java-Programs/src/com/packages/methods/CalculateNetSalary.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class CalculateNetSalary {
 
-	// % = value / total * 100
-	// value = (% * total) / 100
 
 	// Class for Allowances
 	public static class Allowances {
@@ -30,25 +28,34 @@ public class CalculateNetSalary {
 		}
 	}
 
-	// method to calculate total allowance
+    // Method to calculate total allowance based on salary
 	static Allowances totalAllowances(double salary) {
+		
+        // Calculate H.R.A., T.A., and D.A. based on provided percentages
 		double hra = (45.0 / 100) * salary;
 		double ta = (25.0 / 100) * salary;
 		double da = (30.0 / 100) * salary;
-		
+
+        // Create an instance of Allowances with calculated values
 		return new Allowances(hra, ta, da);
 	}
 
-	// method to calculate total deductions
+    // Method to calculate total deductions based on salary
 	static Deductions totalDeductions(double salary) {
+		
+        // Calculate M.A. and P.F. based on provided percentages
 		double ma = (5.0 / 100) * salary;
 		double pf = (25.0 / 100) * salary;
-		
+
+        // Create an instance of Deductions with calculated values
 		return new Deductions(ma, pf);
 	}
 
-	// method to calculate net salary
+	
+    // Method to calculate net salary based on allowances and deductions
 	static double netSalary(Allowances salaryAllowance, Deductions salaryDeductions, double salary) {
+
+		// Calculate total salary by adding basic salary, total allowances, and deducting total deductions
 		double totalSalary = salary + salaryAllowance.totalAllowance - salaryDeductions.totalDeductions;
 		return totalSalary;
 	}
@@ -58,10 +65,13 @@ public class CalculateNetSalary {
 		System.out.println("Enter Employee's Basic Salary : ");
 		double salary = scan.nextDouble();
 
+		// Calculate allowances and deductions
 		Allowances salaryAllowance = totalAllowances(salary);
 		Deductions salaryDeductions = totalDeductions(salary);
 
-		System.out.println("In Hand Salary is : " + netSalary(salaryAllowance, salaryDeductions, salary));
+		// Calculate and display the net salary
+        double netSalary = netSalary(salaryAllowance, salaryDeductions, salary);
+        System.out.println("In Hand Salary is : " + netSalary);
 
 		scan.close();
 	}
@@ -83,7 +93,7 @@ public class CalculateNetSalary {
  * 
  * 2. **Static Inner Class**: - A `static` inner class does not require an
  * instance of the outer class to be created before you can create an instance
- * of the inner class. This allows you to use it in a more standalone manner. -
+ * of the inner class. This allows you to use it in a more stand alone manner. -
  * If the `Allowances` class doesn't need to access any instance-specific data
  * or methods from the outer class, it can be made `static`. This is a good
  * practice if the inner class doesn't have any reliance on the outer class's
@@ -97,3 +107,8 @@ public class CalculateNetSalary {
  * 
  * 
  */
+
+
+
+
+
