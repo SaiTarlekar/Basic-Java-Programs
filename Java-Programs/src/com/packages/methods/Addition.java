58199a9ef56f7242bd87260e89c157125afe4908
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-06 14:20:50 +0530

    Commit

---
+++ Java-Programs/src/com/packages/methods/Addition.java
@@ -0,0 +1,23 @@
+package com.packages.methods;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	static Scanner scan = new Scanner(System.in);
+
+	public static int doAddition(int firstNumber, int secondNumber) {
+		return (firstNumber + secondNumber);
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Enter First Number : ");
+		int firstNumber = scan.nextInt();
+		System.out.println("Enter Second Number : ");
+		int secondNumber = scan.nextInt();
+
+		System.out.println("Addition of Entered Number is : " + doAddition(firstNumber, secondNumber));
+
+	}
+
+}
