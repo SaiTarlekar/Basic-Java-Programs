f792025ef6b71bfb152602500e15f204150ce93e
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-07 10:49:56 +0530

    Commit

---
+++ Java-Programs/src/com/packages/methods/Modulo.java
@@ -0,0 +1,12 @@
+package com.packages.methods;
+import java.util.Scanner;
+
+public class Modulo {
+
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		int num = scan.nextInt();
+		System.out.println(num+ " % 10 = " +num%10);
+	}
+}
