f5d90cdeff4187bc1a28ab79e122d214ad9c8e62
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-07 09:34:42 +0530

    Commit

---
+++ Java-Programs/src/com/packages/methods/DynamicExpressionEvaluator.java
@@ -0,0 +1,27 @@
+package com.packages.methods;
+
+import java.util.Scanner;
+
+public class DynamicExpressionEvaluator {
+	
+	private static Scanner scan = new Scanner(System.in);
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String args[]) {
+
+		
+		closeScanner();
+	}
+	
+	
+	private static void closeScanner() {
+		scan.close();
+	}
+}
