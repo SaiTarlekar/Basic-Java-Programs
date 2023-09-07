76aa0e55811a209969106bf74c7c5e86b48b0a71
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-07 13:59:57 +0530

    Commit

---
+++ Java-Programs/src/com/packages/decisionmaking/NumberComparison.java
@@ -0,0 +1,25 @@
+package com.packages.decisionmaking;
+
+import java.util.Scanner;
+
+public class NumberComparison {
+	static void checkNumberPositivity(int number1, int number2) {
+		if (number1 > number2)
+			System.out.println("Number1 is Greater.");
+		else if(number1 == number2)
+			System.out.println("Numbers are equal");
+		else
+			System.out.print("Number2 is Greater");
+	}
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First Number : ");
+		int number1 = scan.nextInt();
+		
+		System.out.println("Enter Second Number : ");
+		int number2 = scan.nextInt();
+		checkNumberPositivity(number1, number2);
+		
+	}
+}
---
+++ Java-Programs/src/com/packages/decisionmaking/NumberPositivityChecker.java
@@ -0,0 +1,21 @@
+package com.packages.decisionmaking;
+import java.util.Scanner;
+public class NumberPositivityChecker {
+
+	static void checkNumberPositivity(int num) {
+		if (num > 0)
+			System.out.println("Number is Positive.");
+		else if(num < 0)
+			System.out.println("Number is Negative");
+		else
+			System.out.print("Number is Zero");
+	}
+	
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Number : ");
+		int num = scan.nextInt();
+		checkNumberPositivity(num);
+		
+	}
+}
