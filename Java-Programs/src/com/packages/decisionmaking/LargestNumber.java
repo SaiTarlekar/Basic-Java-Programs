7561a389d583e95e2db2be0dd69aa788e7e68c10
SaiTarlekar <onlyforimages7@gmail.com>
2023-09-12 18:38:02 +0530

    Commit

---
+++ Java-Programs/src/com/packages/decisionmaking/LargestNumber.java
@@ -0,0 +1,79 @@
+package com.packages.decisionmaking;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+	static Scanner scan = new Scanner(System.in);
+
+	static void findLargestNumber(int firstNumber, int secondNumber, int thirdNumber) {
+
+		if ((firstNumber == secondNumber) && (secondNumber == thirdNumber)) {
+			System.out.println("All three Numbers are Equal.");
+		}
+
+		else if ((firstNumber == secondNumber) && (secondNumber != thirdNumber)) {
+			if (firstNumber > thirdNumber)
+				System.out.println("First Number (" + firstNumber + ") & Second Number (" + secondNumber + ") "
+						+ "are same and largest.");
+			else
+				System.out.println("Third Number (" + thirdNumber + ") is largest.");
+		}
+
+		else if ((secondNumber == thirdNumber) && (firstNumber != secondNumber)) {
+			if (thirdNumber > firstNumber)
+				System.out.println("Second Number (" + secondNumber + ") & Third Number (" + thirdNumber + ") "
+						+ "are same and largest.");
+			else
+				System.out.println("First Number (" + firstNumber + ") is largest.");
+		}
+
+		else if ((firstNumber == thirdNumber) && (firstNumber != secondNumber)) {
+			if (firstNumber > secondNumber)
+				System.out.println("First Number (" + firstNumber + ") & Third Number (" + thirdNumber + ") "
+						+ "are same and largest.");
+			else
+				System.out.println("Second Number (" + secondNumber + ") is largest.");
+		}
+
+		else {
+			if (firstNumber > secondNumber) {
+				if (firstNumber > thirdNumber)
+					System.out.println("First Number is largest.");
+				else
+					System.out.println("Third Number is largest.");
+			} else {
+				if (secondNumber > thirdNumber)
+					System.out.println("Second Number is largest.");
+				else
+					System.out.println("Third Number is largest.");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter First Number : ");
+		int firstNumber = scan.nextInt();
+		System.out.println("Enter Second Number : ");
+		int secondNumber = scan.nextInt();
+		System.out.println("Enter Third Number : ");
+		int thirdNumber = scan.nextInt();
+
+//		System.out.println(+findLargestNumber(firstNumber, secondNumber, thirdNumber));
+		findLargestNumber(firstNumber, secondNumber, thirdNumber);
+	}
+}
+
+/*
+ * if(firstNumber > secondNumber) { if(firstNumber > thirdNumber)
+ * System.out.println("First Number is largest."); else
+ * System.out.println("Third Number is largest."); } else { if(secondNumber >
+ * thirdNumber) System.out.println("Second Number is largest."); else
+ * System.out.println("Third Number is largest."); }
+ * 
+ * 
+ * 
+ * return (firstNumber > secondNumber) ? ((firstNumber > thirdNumber) ?
+ * firstNumber : thirdNumber) : ((secondNumber > thirdNumber) ? secondNumber :
+ * thirdNumber);
+ */
--- Java-Programs/src/com/packages/decisionmaking/ResultDecider.java
+++ Java-Programs/src/com/packages/decisionmaking/ResultDecider.java
@@ -1,5 +1,24 @@
 package com.packages.decisionmaking;
 
-public class ResultDecider {
+import java.util.Scanner;
 
+public class ResultDecider {
+	static Scanner scan = new Scanner(System.in);
+
+	static void resultDecider(float marks) {
+		if (marks >= 35 && marks <= 100)
+			System.out.println("Pass");
+		else if (marks >= 0 && marks < 35)
+			System.out.println("Fail");
+		else
+			System.out.println("Incorrect Marks");
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter Marks : ");
+		float marks = scan.nextFloat();
+		resultDecider(marks);
+
+	}
 }
