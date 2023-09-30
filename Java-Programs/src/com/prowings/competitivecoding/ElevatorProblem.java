4d396b9ec305ee50fb542660bd09a21ada1fcd3e
SaiTarlekar <onlyforimages7@gmail.com>
2023-10-01 03:13:18 +0530

    Commit

--- Java-Programs/src/com/prowings/competitivecoding/ElevatorProblem.java
+++ Java-Programs/src/com/prowings/competitivecoding/ElevatorProblem.java
@@ -8,18 +8,24 @@ import java.util.Scanner;
 
 public class ElevatorProblem {
 
+	public static final int MIN_FLOOR = 0;
+	public static final int MAX_FLOOR = 1000;
+	public static final int BUTTON_PRESS = 50;
 	static Scanner scan = new Scanner(System.in);
 	
+	// no-arg constructor.
 	public ElevatorProblem() {}
 	
+	// Method to read current floor as input.
 	public static int getCurrentFloor() { 
 		int currentFloor = 0;
 		System.out.println("Enter Current Floor (0 to 1000) : ");
 		
 		try {
-			int num = scan.nextInt();
-			if(num>=0 && num<=1000)
-				currentFloor = num;
+			int checkFloor = scan.nextInt();
+			// Verifying if the entered floor number is between the limit of building.
+			if(checkFloor>=0 && checkFloor<=1000)
+				currentFloor = checkFloor;
 			else
 				throw new IllegalArgumentException("Input must be between 0 and 1000(Inclusive)");
 		}
@@ -31,15 +37,16 @@ public class ElevatorProblem {
 		return currentFloor;
 	}
 	
+	// Method to read direction as input.
 	public static int getDirection() {
 		int currentDirection=0;	
 		System.out.println("Enter Direction of Elevator (1 for upward and -1 for downward) :");
 		
 		try {
-			int num = scan.nextInt();
+			int checkDirection = scan.nextInt();
 			scan.nextLine();
-			if (num == -1 || num == 1)
-				currentDirection = num;
+			if (checkDirection == -1 || checkDirection == 1)
+				currentDirection = checkDirection;
 			else
 				throw new IllegalArgumentException("Input must be either -1 or 1.");
 		}		
@@ -52,20 +59,21 @@ public class ElevatorProblem {
 		return currentDirection;
 	}
 	
+	// Method to read various input buttons pressed by users to reach their destination.
 	public static List<Integer> getButtonsPressed() {
 		int count=0, buttonPressed=0;
 		List<Integer> buttonPressedList = new ArrayList<>();
 		System.out.println("You can now enter the lift : ");
 		System.out.println("Enter Button (Press c to close the lift): ");
 		while(scan.hasNextInt()) {
-			int num = scan.nextInt();
+			int checkButton = scan.nextInt();
 			try {
-				if (num>=0 && num<=1000) {
+				if (checkButton>=MIN_FLOOR && checkButton<=MAX_FLOOR) {
 					count++;
-					buttonPressed = num;
+					buttonPressed = checkButton;
 					
 					try {
-						if(count>50)
+						if(count>BUTTON_PRESS)
 							throw new ArrayIndexOutOfBoundsException("Lift Limit Exceeded.");
 					}
 					catch(Exception e) {
@@ -96,6 +104,7 @@ public class ElevatorProblem {
 		return distinctButtonsPressed;
 	}
 
+	//Method to find the halts taken by lift.
 	public static List<Integer> ElevatorHalt(int currentDirection, int currentFloor, List<Integer> buttonsPressed){
 		// upward list
 		List<Integer> upwardHalts = new ArrayList<>();
@@ -124,6 +133,7 @@ public class ElevatorProblem {
 		
 	}
 	
+	
 	public static void main(String[] args) {
 		int currentFloor = getCurrentFloor();
 		int currentDirection = getDirection();
@@ -133,10 +143,11 @@ public class ElevatorProblem {
 		result = ElevatorHalt(currentDirection, currentFloor, buttonPressedList);
 		
 		System.out.println("Result : ");
-		for(Integer num:result)
-			System.out.print(" "+num+" ");
+		for(Integer floor:result)
+			System.out.print(" "+floor+" ");
 	}
 
+	// Method to close Scanner
 	static void scannerClose() {
 		scan.close();
 	}
