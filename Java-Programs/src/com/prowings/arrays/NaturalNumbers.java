cc81e4a7740217c3d2510f3d075b954111ed56c4
SaiTarlekar <onlyforimages7@gmail.com>
2023-10-03 13:48:48 +0530

    Commit

---
+++ Java-Programs/src/com/prowings/arrays/NaturalNumbers.java
@@ -0,0 +1,12 @@
+package com.prowings.arrays;
+
+import java.util.Scanner;
+
+public class NaturalNumbers {
+
+	private static Scanner scan = new Scanner(System.in);
+	
+	public NaturalNumbers() {
+	}
+
+}
