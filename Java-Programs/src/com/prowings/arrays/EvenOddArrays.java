07342afe59f3ec49afd5a2e36306a5c7b35da7bf
SaiTarlekar <onlyforimages7@gmail.com>
2023-10-03 14:02:40 +0530

    Commit

---
+++ Java-Programs/src/com/prowings/arrays/EvenOddArrays.java
@@ -0,0 +1,21 @@
+/*
+Even and Odd Numbers:
+Create a program that separates even and odd numbers from an array using a for loop and two separate arrays.
+*/
+
+
+package com.prowings.arrays;
+import java.util.Scanner;
+
+public class EvenOddArrays {
+
+	private static Scanner scan = new Scanner(System.in);
+	public EvenOddArrays() {
+	
+	}
+
+	public static void main(String[] args) {
+	
+	}
+
+}
