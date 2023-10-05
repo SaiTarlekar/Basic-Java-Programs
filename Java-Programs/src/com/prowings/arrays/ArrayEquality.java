0934167381b5a695b2b4dd19fa9a27ef1b392100
SaiTarlekar <onlyforimages7@gmail.com>
2023-10-06 00:51:57 +0530

    Commit

--- Java-Programs/src/com/prowings/arrays/ArrayEquality.java
+++ Java-Programs/src/com/prowings/arrays/ArrayEquality.java
@@ -2,36 +2,33 @@ package com.prowings.arrays;
 
 public class ArrayEquality {
 
-	public ArrayEquality() {}
+	public ArrayEquality() {
+	}
 
-	
 	static boolean checkEquality(int[] nums, int[] nums1) {
-	
+
 		boolean isEqual = false;
-		
-		if(nums.length == nums1.length) {
-			for(int i=0; i<nums.length; i++) {
-				if(nums[i] == nums1[i]) {
+
+		if (nums.length == nums1.length) {
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] == nums1[i]) {
 					isEqual = true;
-				}
-				else {
+				} else {
 					isEqual = false;
 					return isEqual;
 				}
-			}			
+			}
 		}
-		
-		
-		return isEqual;
-		
-	}
-	
-	public static void main(String[] args) {
-		int[] nums = {1,2,4};		
-		int[] nums1 = {1,2,3};
 
-		
-		if(checkEquality(nums, nums1))
+		return isEqual;
+
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 4 };
+		int[] nums1 = { 1, 2, 3 };
+
+		if (checkEquality(nums, nums1))
 			System.out.println("Equal");
 		else
 			System.out.println("Not Equal");
