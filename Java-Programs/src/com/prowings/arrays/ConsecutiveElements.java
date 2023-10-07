659941fa6c2e56707afaa78b0be7abc09810f5c4
SaiTarlekar <onlyforimages7@gmail.com>
2023-10-07 14:31:05 +0530

    Commit

--- Java-Programs/src/com/prowings/arrays/ConsecutiveElements.java
+++ Java-Programs/src/com/prowings/arrays/ConsecutiveElements.java
@@ -29,3 +29,36 @@ public class ConsecutiveElements {
 //Sample array: [49, 1, 3, 200, 2, 4, 70, 5]
 //The longest consecutive elements sequence is [1, 2, 3, 4, 5], therefore the program 
 //will return its length 5. 
+
+
+
+
+
+/*
+function findLongestConsecutiveSequence(arr):
+    if arr is empty:
+        return empty sequence
+    
+    # Sort the input array
+    sort(arr)
+    
+    currentSequence = [arr[0]]  # Initialize the current sequence with the first element
+    longestSequence = [arr[0]]  # Initialize the longest sequence with the first element
+    
+    for i from 1 to len(arr) - 1:
+        # Check if the current element is consecutive to the last element in the current sequence
+        if arr[i] == currentSequence[-1] + 1:
+            currentSequence.append(arr[i])  # Add the current element to the current sequence
+        else:
+            # If not consecutive, check if the current sequence is longer than the longest sequence
+            if len(currentSequence) > len(longestSequence):
+                longestSequence = currentSequence  # Update the longest sequence
+            
+            currentSequence = [arr[i]]  # Start a new current sequence with the current element
+    
+    # Check if the last current sequence is longer than the longest sequence
+    if len(currentSequence) > len(longestSequence):
+        longestSequence = currentSequence
+    
+    return longestSequence
+*/
