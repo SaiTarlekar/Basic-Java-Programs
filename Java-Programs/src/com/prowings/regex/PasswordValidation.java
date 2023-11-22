363fa2ab3f67ca9a2bee6a08767b1fb4c63b1bbc
SaiTarlekar <onlyforimages7@gmail.com>
2023-11-22 20:35:40 +0530

    Commit

--- Java-Programs/src/com/prowings/regex/PasswordValidation.java
+++ Java-Programs/src/com/prowings/regex/PasswordValidation.java
@@ -6,9 +6,17 @@ class PasswordValidation
 	
 	public static String validate(String s){
 		//^(?=.[0-9])(?=.[a-z])(?=.[A-Z])(?=.[@#$%^&-+=()])(?=\\S+$).{8, 20}$
+		
+		
+		
 //		^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{8, 20}$
 		
-		Pattern p = Pattern.compile("^(.*[a-z])(.*[A-Z])(.*[0-9])$");
+//		^.(?=.*[0-9a-zA-Z]){8,20}$
+		
+//		^(.*[a-z])(.*[A-Z])(.*[0-9])$
+		
+		
+		Pattern p = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#]).{8,20}$");		
 		Matcher m = p.matcher(s);
 		
 		if(m.find() && m.group().equals(s))
@@ -21,6 +29,6 @@ class PasswordValidation
 
 	public static void main(String args[])
 	{
-			System.out.println(validate("P@ssw0rd"));
+			System.out.println(validate("p@ssw0rd"));
 	}
 }
