27d99d87a2cd752594b7cff6c6c2c91c17534b97
SaiTarlekar <onlyforimages7@gmail.com>
2023-12-02 18:45:26 +0530

    Stack

--- Java-Programs/src/com/prowings/stack/DecimalToBinary.java
+++ Java-Programs/src/com/prowings/stack/DecimalToBinary.java
@@ -11,20 +11,24 @@ public class DecimalToBinary {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter Decimal Number : ");
+		System.out.print("Enter Decimal Number : ");
 		int number = scan.nextInt();
 		
-		Stack s = new Stack(String.valueOf(number).length()*10);
+		int bits = (int) (Math.log(number) / Math.log(2) + 1);
+		Stack s = new Stack(bits);
+		
 		int counter = 0;
-		while(number > 0) {
-			s.push(number%2);
-			number = number/2;
+		int temp = number;
+		while(temp > 0) {
+			s.push(temp%2);
+			temp = temp/2;
 			counter++;
 		}
 		
+		System.out.println("\n\n("+number+") base 10 : " +number);
+		System.out.print("("+number+") base 2 : ");
 		for(int i=0; i<counter; i++) {
 			System.out.print(s.pop()+ " ");
 		}
-	
 	}
 }
