1040cefbeb88a2b9a7262d7441a797196028047a
SaiTarlekar <onlyforimages7@gmail.com>
2023-12-15 07:33:51 +0530

    Array Assignment

--- Java-Programs/src/com/prowings/array_assignment/ShufflingElements.java
+++ Java-Programs/src/com/prowings/array_assignment/ShufflingElements.java
@@ -10,8 +10,6 @@ public class ShufflingElements {
 
 	public static void main(String[] args) {
 		int[] nums = { 1, 2 , 3, 4 };
-		
-		
 		System.out.println("Shuffled Array  : "+Arrays.toString(doShuffling(nums)));
 	}
 
